8cb9e6f26141a7af712ed74407e7c10587a25796
jy9307 <genitor@naver.com>
2025-03-18 09:53:56 +0900

    1차 수정

---
+++ streamlit_practice/6_navigation.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+### 아이콘은 다음 페이지에서 마음대로 가져다쓸 수 있습니다
+### https://fonts.google.com/icons?icon.set=Material+Symbols&icon.style=Rounded
+write_page = st.Page("pages/1_write.py", title="글쓰기 페이지", icon=":material/edit_document:")
+button_page = st.Page("pages/2_button.py", title="버튼 페이지", icon=":material/switch:")
+
+pg = st.navigation([write_page, button_page])
+
+pg.run()
